119af48610be2b0aa04a3827044413b13b1a8464
Jon Croce <crocedev@gmail.com>
2023-09-03 13:02:31 -0400

    initial commit

---
+++ src/slides.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Presentation, Slide } from '@components';
+	import { gsap } from 'gsap';
+	import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		gsap.registerPlugin(MotionPathPlugin);
+	});
+</script>
+
+<Presentation>
+	<!-- 1 -->
+	<Slide>
+		<p class="font-bold">⏳ Timing and Spacing Demo ⌛</p>
+	</Slide>
+</Presentation>
