3abea0f957ddf90d88248ffe8b06cb79e26063bf
Jon Croce <crocedev@gmail.com>
2023-09-04 00:47:19 -0400

    update slides

--- src/slides.svelte
+++ src/slides.svelte
@@ -1,17 +1,45 @@
 <script lang="ts">
-	import { Presentation, Slide } from '@components';
-	import { gsap } from 'gsap';
-	import { MotionPathPlugin } from 'gsap/MotionPathPlugin';
 	import { onMount } from 'svelte';
+	import {
+		Presentation,
+		Slide,
+		EaseDemo,
+		AnimationControls,
+	} from '@components';
+	import {
+		createAnimations,
+		labelByAnimationName,
+		type AnimationName,
+	} from '@lib/animations';
+
+	let animations: Record<AnimationName, gsap.core.Timeline>;
 
 	onMount(() => {
-		gsap.registerPlugin(MotionPathPlugin);
+		animations = createAnimations();
 	});
+
+	function playAnimation(animationName: AnimationName) {
+		animations[animationName].play(0);
+	}
+
+	function resetAnimation(animationName: AnimationName) {
+		animations[animationName].pause(0);
+	}
 </script>
 
 <Presentation>
-	<!-- 1 -->
 	<Slide>
 		<p class="font-bold">⏳ Timing and Spacing Demo ⌛</p>
 	</Slide>
+	{#each labelByAnimationName.entries() as [animationName, label]}
+		<Slide animate on:in={() => resetAnimation(animationName)}>
+			<h2 class="my-12 font-bold">{label}</h2>
+			<EaseDemo {animationName} />
+			<AnimationControls
+				{animationName}
+				play={playAnimation}
+				reset={resetAnimation}
+			/>
+		</Slide>
+	{/each}
 </Presentation>
