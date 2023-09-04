1bc7e19672b44214aa77d60ba78cd3fed58d592f
Jon Croce <crocedev@gmail.com>
2023-09-04 15:58:32 -0400

    add dep carbon-components-svelte

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import Slides from './slides.svelte'
-import '@styles/tailwind.css'
+import Slides from './slides.svelte';
+import '@styles/tailwind.css';
+import 'carbon-components-svelte/css/g90.css';
 
 const app = new Slides({
 	target: document.getElementById('app'),
-})
+});
 
-export default app
+export default app;
