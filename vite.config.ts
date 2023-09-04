50618fc4c3d7b1326e72b2826994216152f8bdfb
Jon Croce <crocedev@gmail.com>
2023-09-04 02:25:25 -0400

    set base property in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-import path from 'path'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,4 +15,5 @@ export default defineConfig({
 			'@styles': path.resolve(__dirname, './src/lib/styles'),
 		},
 	},
-})
+	base: '/timing-and-spacing-demo/'
+});
